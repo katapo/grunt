c2653d9c37ae0d38f8f6aae1f26695cc3fab14c9
katapo <kata.pochec@gmail.com>
2017-09-20 20:59:30 +0200

    modified .gitignore and Gruntfile.js added images file and deleted style.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({ 
-  	sass: {
+    sass: {
       options: {
         sourceMap: true
       },
@@ -14,43 +14,46 @@ module.exports = function(grunt) {
     },
 
     imagemin: {
-	    dynamic: {
-	        files: [{
-	            expand: true,
-	            cwd: 'images/',
-	            src: ['**/*.{png,jpg,gif}'],
-	            dest: 'images/build/'
-	        }]
-	    }
-	},
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'images/',
+              src: ['**/*.{png,jpg,gif}'],
+              dest: 'images/build/'
+          }]
+      }
+  },
 
-	watch: {
-    	scripts: {
-        	files: ['sass/*.sass'],
-        	tasks: ['sass'],
-        	options: {
-            	spawn: false,
-        	},
-    	}
-	},
+  watch: {
+      scripts: {
+          files: ['*.sass'],
+          tasks: ['sass'],
+          options: {
+              spawn: false,
+          },
+      }
+  },
 
-	browserSync: {
-    	bsFiles: {
-        	src : 'assets/css/*.css'
-    	},
-    	options: {
-        	server: {
-            	baseDir: "./"
-        	}
-    	}
-	}
+  browserSync: {
+      dev: {
+          bsFiles: {
+              src : [
+                     'css/*.css',
+                     '*.html'
+              ]
+            },
+          options: {
+              watchTask: true,
+          }
+      }
+  }
 
   });
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');  
   grunt.loadNpmTasks('grunt-browser-sync');
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch', 'browserSync']);
+  grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
 };
